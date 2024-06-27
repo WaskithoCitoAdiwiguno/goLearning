391b993b163c1a5c84926cf5a44bc539e9f670d2
Waskitho Cito Adiwiguno <114119615+WaskithoCitoAdiwiguno@users.noreply.github.com>
2024-06-27 14:39:31 +0700

    Delete goLearning directory

--- goLearning/model/course/course.go
+++
@@ -1,7 +0,0 @@
-package course
-
-type Course struct {
-	CourseId int `json:"course_id"`
-	CourseName string `json:"course_name"`
-	Teacher string `json:"teacher"`
-}
--- goLearning/model/user/user.go
+++
@@ -1,8 +0,0 @@
-package user
-
-type User struct {
-	UserId int `json:"user_id"`
-	Username string `json:"username"`
-	Email string `json:"email"`
-	Password string `json:"password"`
-}
